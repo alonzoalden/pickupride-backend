fdf77029b5b1e924428c94535ec3a92ad8d87aa0
Alonzo Alden <alonzoalden@gmail.com>
2017-11-26 07:54:17 -0800

    Set up node config and passport.js config

--- app.js
+++ app.js
@@ -9,28 +9,66 @@ var methods = require('methods');
 var mongoose = require('mongoose');
 var key = require('./env-config.js');
 
-//connect to MongoDB MLABS database
-mongoose.connect('mongodb://' + key.DB_USERNAME + ':' + key.DB_PASSWORD + key.DB_URL);
-
 var app = express();
 
 app.use(cors());
-app.use(cookieParser());
 
+// Normal express config defaults
 app.use(require('morgan')('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(require('method-override')());
 
+app.use(require('method-override')());
 app.use(express.static(__dirname + '/dist'));
 
+app.use(session({ secret: 'conduit', cookie: { maxAge: 60000 }, resave: false, saveUninitialized: false  }));
+
+if (!isProduction) {
+  app.use(errorhandler());
+}
+
+if(isProduction){
+  mongoose.connect(process.env.MONGODB_URI);
+} else {
+  mongoose.connect('mongodb://' + key.DB_USERNAME + ':' + key.DB_PASSWORD + key.DB_URL);
+  mongoose.set('debug', true);
+}
+
+require('./config/passport');
 app.use(require('./routes'));
 
-// catch 404 and forward to error handler
+/// catch 404 and forward to error handler
 app.use(function(req, res, next) {
-    var err = new Error('Not Found :()');
-    err.status = 404;
-    next(err);
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+/// error handlers
+
+// development error handler
+// will print stacktrace
+if (!isProduction) {
+  app.use(function(err, req, res, next) {
+    console.log(err.stack);
+
+    res.status(err.status || 500);
+
+    res.json({'errors': {
+      message: err.message,
+      error: err
+    }});
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.json({'errors': {
+    message: err.message,
+    error: {}
+  }});
 });
 
 app.listen(3000, function () {
